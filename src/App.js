3768c28969a2098d1591373f547abf45cad9b776
huycan <vohuy21032001@gmail.com>
2022-06-26 17:09:51 +0700

    add component ScrollToTop

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './page/Home/Home';
 import Login from './page/Login/Login';
 import SignUp from './page/SignUp/SignUp';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import './App.css';
 
-const App = () => (
-    <BrowserRouter>
-        <div className='app'>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<SignUp />} />
-            </Routes>
-        </div>
-    </BrowserRouter>
-);
-
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<SignUp />} />
+                </Routes>
+                <ScrollToTop />
+            </div>
+        </BrowserRouter>
+    );
+};
 export default App;
